619f4d79953875f399fa8dfac77da6c9174b37b2
Stephen Coward <stephendcoward@gmail.com>
2015-01-19 01:55:02 -0600

    Remove reference to documents Our datastore doesn't have to be a document database, remove all mention of document from the code.  Also add code to create a server and to run it, basically just starting to handle NPEs and other errors.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-require("./livedata_server.js");
+var livedata = require("./livedata_server.js");
 
 console.log("Alive? ", (DDPServer !== null));
+
+console.log("Attempting to start server, here goes nothing:\n\n");
+
+var server = new livedata.Server;
